368a192f2dcdcaac42b64d64bec7e5c92ed3d4ba
linhhut <linhhn@vega.com.vn>
2017-10-18 15:32:12 +0700

    finish the hero editor tut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input placeholder="name">
+    </div>
+  `
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  // hero = 'Windstorm';
+  hero: Hero = {
+    id : 1,
+    name: 'Windstorm'
+  };
 }
