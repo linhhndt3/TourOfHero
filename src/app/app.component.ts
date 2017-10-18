2cfd1017bf3d0b988e176cfef925a0fafb05de75
linhhut <linhhn@vega.com.vn>
2017-10-18 14:27:00 +0700

    Show the hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
 }
